5b3f3c617e80035135344c2734eb735392949a11
jackgo <jackgo2080@gmail.com>
2023-05-21 15:46:59 +0800

    0

--- train.py
+++ train.py
@@ -32,7 +32,7 @@ class TrainingArguments(transformers.TrainingArguments):
     cache_dir: Optional[str] = field(default=None)
     optim: str = field(default="adamw_torch")
     model_max_length: int = field(
-        default=256,
+        default=128,
         metadata={"help": "Maximum sequence length. Sequences will be right padded (and possibly truncated)."},
     )
 
